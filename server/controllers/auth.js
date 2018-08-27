ba4f15b9bcb6739d6675e3ac60e2873379200d2e
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-08-27 22:48:42 +0300

    Log in functionality

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,14 +1,39 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const keys = require('../keys');
 
 const User = require('../models/User');
 
-module.exports.login = function (req, res) {
-  res.status(200).json({
-    login: {
-      email: req.body.email,
-      password: req.body.password
+module.exports.login = async function (req, res) {
+  const candidate = await User.findOne({
+    email: req.body.email
+  });
+
+  if (candidate) {
+    const passResult = bcrypt.compareSync(req.body.password, candidate.password);
+
+    if (passResult) {
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {
+          expiresIn: 60 * 60
+        });
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      });
+    } else {
+      res.status(401).json({
+        message: 'Passwords do not match'
+      });
     }
-  })
+  } else {
+    res.status(404).json({
+      message: 'User not found'
+    });
+  }
 }
 
 module.exports.register = async function (req, res) {
--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUri: 'mongodb://admin0:admin0@ds235302.mlab.com:35302/fullstack'
+  mongoUri: 'mongodb://admin0:admin0@ds235302.mlab.com:35302/fullstack',
+  jwt: 'dev'
 };
