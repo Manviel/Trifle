485103137d2b47636733dcb223e9178a65362cbc
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-08-29 11:53:57 +0300

    Order functionality

--- server/routes/position.js
+++ server/routes/position.js
@@ -4,12 +4,12 @@ const controller = require('../controllers/position');
 
 const router = express.Router();
 
-router.get('/:category', controller.getByCategory);
+router.get('/:category', passport.authenticate('jwt', { session: false }), controller.getByCategory);
 
-router.post('/', controller.create);
+router.post('/', passport.authenticate('jwt', { session: false }), controller.create);
 
-router.patch('/:id', controller.update);
+router.patch('/:id', passport.authenticate('jwt', { session: false }), controller.update);
 
-router.delete('/:id', controller.remove);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), controller.remove);
 
 module.exports = router;
