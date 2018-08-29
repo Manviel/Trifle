5cd3b3d39b2fb1a487f2a2950da39825fcc611dc
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-08-29 12:47:12 +0300

    Concurrently launch client and server

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const controller = require('../controllers/auth');
 
 const router = express.Router();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { InMemoryDataService }  from './in-memory-data.service';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
@@ -20,7 +15,6 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
     DashboardComponent,
     HeroSearchComponent
   ],
@@ -28,12 +22,9 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    HttpClientModule
   ],
-  providers: [HeroService, MessageService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
