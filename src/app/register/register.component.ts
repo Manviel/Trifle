d7e4d9f7999e071481284a189c6f110956f5c460
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-09-01 12:03:26 +0300

    Register service

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  form: FormGroup
+  sub: Subscription
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
+  onSubmit() {
+    this.form.disable();
+
+    this.sub = this.auth.register(this.form.value).subscribe(
+      () => this.router.navigate(['/login'], {
+        queryParams: {
+          registered: true
+        }
+      }),
+      () => this.form.enable()
+    );
   }
 
 }
