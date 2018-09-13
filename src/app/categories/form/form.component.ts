f4b66c5b483a691a1b1dce57c497f4762c2b03d3
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-09-13 22:33:07 +0300

    Handling form group

--- src/app/categories/form/form.component.ts
+++ src/app/categories/form/form.component.ts
@@ -1,22 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material';
+
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+import { CategoriesService } from '../../services/categories.service';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+	selector: 'app-form',
+	templateUrl: './form.component.html',
+	styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
 
-  isNew = true
+	form: FormGroup
+	isNew = true
 
-  constructor(private route: ActivatedRoute) { }
+	constructor(
+		private route: ActivatedRoute,
+		private categories: CategoriesService,
+		public snackBar: MatSnackBar
+	) { }
 
-  ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      if (params['id']) {
-        this.isNew = false;
-      }
-    })
-  }
+	ngOnInit() {
+		this.form = new FormGroup({
+			name: new FormControl(null, Validators.required)
+		});
+
+		this.route.params.pipe(
+			switchMap((params: Params) => {
+				if (params['id']) {
+					this.isNew = false;
+
+					return this.categories.getById(params['id']);
+				}
+
+				return of(null);
+			})
+		).subscribe(category => {
+			if (category) {
+				this.form.patchValue({
+					name: category.name
+				})
+			}
+
+			this.form.enable();
+		},
+			err => this.snackBar.open(err.error.message)
+		)
+	}
+
+	onSubmit() { }
 }
