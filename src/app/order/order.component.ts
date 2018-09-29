9b5fe5e45f1c06c6e83760ff32447be2a22fcbe8
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2018-09-29 23:58:31 +0300

    Сhange the title in the transition

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { MatDialog } from '@angular/material';
+
+import { DialogComponent } from '../dialog/dialog.component';
+
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
-  styleUrls: ['./order.component.css']
+  styleUrls: ['./order.component.css'],
+  providers: [OrderService]
 })
 export class OrderComponent implements OnInit {
+  isRoot: boolean
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private orderService: OrderService,
+    public dialog: MatDialog,
+  ) { }
 
-  ngOnInit() {
+  openModal(): void {
+    this.dialog.open(DialogComponent);
   }
 
+  ngOnInit() {
+    this.isRoot = this.router.url === '/order';
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.isRoot = this.router.url === '/order';
+      }
+    });
+  }
 }
