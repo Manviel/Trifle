945644e1a73c49d2a996dbba99e5252c0e038415
Manviel <Mihaylo.Merezhko@kname.edu.ua>
2019-02-23 21:49:16 +0200

    Creating an overview page

--- src/app/interfaces/interface.ts
+++ src/app/interfaces/interface.ts
@@ -1,45 +1,57 @@
 export interface User {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
 export interface Category {
-  name: string
-  image?: string
-  user?: string
-  _id?: string
+  name: string;
+  image?: string;
+  user?: string;
+  _id?: string;
 }
 
 export interface Message {
-  message: string
+  message: string;
 }
 
 export interface Position {
-  name: string
-  cost: number
-  user?: string
-  category: string
-  _id?: string
-  quantity?: number
+  name: string;
+  cost: number;
+  user?: string;
+  category: string;
+  _id?: string;
+  quantity?: number;
 }
 
 export interface Order {
-  date?: Date
-  order?: number
-  user?: string
-  list: ListItem[]
-  _id?: string
+  date?: Date;
+  order?: number;
+  user?: string;
+  list: ListItem[];
+  _id?: string;
 }
 
 export interface ListItem {
-  name: string
-  cost: number
-  quantity: number
-  _id?: string
+  name: string;
+  cost: number;
+  quantity: number;
+  _id?: string;
 }
 
 export interface Filter {
-  start?: Date
-  end?: Date
-  order?: number
+  start?: Date;
+  end?: Date;
+  order?: number;
+}
+
+export interface OverviewPage {
+  orders: OverviewItem;
+  gain: OverviewItem;
+}
+
+export interface OverviewItem {
+  percent: number;
+  compare: number;
+  yesterday: number;
+  isHigher: boolean;
 }
--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AnalyticsService } from '../services/analytics.service';
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
+  data: any;
 
-  constructor() { }
+  yesterday = new Date();
 
-  ngOnInit() { }
+  constructor(private service: AnalyticsService) {}
+
+  ngOnInit() {
+    this.service.getOverview().subscribe(res => {
+      this.data = res;
+    });
+
+    this.yesterday.setDate(this.yesterday.getDate() - 1);
+  }
 }
